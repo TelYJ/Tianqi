be0beab107319acc005bc62ae861888d1852b993
TelYJ <114336083+TelYJ@users.noreply.github.com>
2022-10-13 23:40:11 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 import random
+# import datetime
 from time import localtime
 from requests import get, post
 from datetime import datetime, date
 from zhdate import ZhDate
 import sys
 import os
- 
- 
+
+
 def get_color():
     # 获取随机颜色
     get_colors = lambda n: list(map(lambda i: "#" + "%06x" % random.randint(0, 0xFFFFFF), range(n)))
     color_list = get_colors(100)
     return random.choice(color_list)
- 
- 
+
+
 def get_access_token():
     # appId
     app_id = config["app_id"]
@@ -29,8 +30,9 @@ def get_access_token():
         sys.exit(1)
     # print(access_token)
     return access_token
- 
- 
+
+
+# 获取天气
 def get_weather(region):
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
@@ -59,8 +61,9 @@ def get_weather(region):
     # 风向
     wind_dir = response["now"]["windDir"]
     return weather, temp, wind_dir
- 
- 
+
+
+# 获取生日
 def get_birthday(birthday, year, today):
     birthday_year = birthday.split("-")[0]
     # 判断是否为农历生日
@@ -78,7 +81,7 @@ def get_birthday(birthday, year, today):
         birthday_day = birthday.day
         # 今年生日
         year_date = date(year, birthday_month, birthday_day)
- 
+
     else:
         # 获取国历生日的今年对应月和日
         birthday_month = int(birthday.split("-")[1])
@@ -100,8 +103,8 @@ def get_birthday(birthday, year, today):
         birth_date = year_date
         birth_day = str(birth_date.__sub__(today)).split(" ")[0]
     return birth_day
- 
- 
+
+
 def get_ciba():
     url = "http://open.iciba.com/dsapi/"
     headers = {
@@ -113,8 +116,9 @@ def get_ciba():
     note_en = r.json()["content"]
     note_ch = r.json()["note"]
     return note_ch, note_en
- 
- 
+
+
+# main send
 def send_message(to_user, access_token, region_name, weather, temp, wind_dir, note_ch, note_en):
     url = "https://api.weixin.qq.com/cgi-bin/message/template/send?access_token={}".format(access_token)
     week_list = ["星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"]
@@ -122,12 +126,18 @@ def send_message(to_user, access_token, region_name, weather, temp, wind_dir, no
     month = localtime().tm_mon
     day = localtime().tm_mday
     today = datetime.date(datetime(year=year, month=month, day=day))
+    print(today)
     week = week_list[today.isoweekday() % 7]
     # 获取在一起的日子的日期格式
     love_year = int(config["love_date"].split("-")[0])
     love_month = int(config["love_date"].split("-")[1])
     love_day = int(config["love_date"].split("-")[2])
     love_date = date(love_year, love_month, love_day)
+    # 获取最初姨妈日期
+    aunt_year = int(config["aunt_data"].split("-")[0])
+    aunt_month = int(config["aunt_data"].split("-")[1])
+    aunt_day = int(config["aunt_data"].split("-")[2])
+    aunt_date = datetime.date(datetime(year=aunt_year, month=aunt_month, day=aunt_day))
     # 获取在一起的日期差
     love_days = str(today.__sub__(love_date)).split(" ")[0]
     # 获取所有生日数据
@@ -184,6 +194,36 @@ def send_message(to_user, access_token, region_name, weather, temp, wind_dir, no
             birthday_data = "距离{}的生日还有{}天".format(value["name"], birth_day)
         # 将生日数据插入data
         data["data"][key] = {"value": birthday_data, "color": get_color()}
+    # 姨妈Coming逻辑
+    aunt_year = int(config["aunt_data"].split("-")[0])
+    aunt_month = int(config["aunt_data"].split("-")[1])
+    aunt_day = int(config["aunt_data"].split("-")[2])
+    aunt_date = datetime.date(datetime(year=aunt_year, month=aunt_month, day=aunt_day))
+    # print(datetime.combine(aunt_date, time()))
+    # add by 20221011
+    dis_month = 2
+    # 姨妈逻辑
+    # 根据当前时间判断加多少个28天
+    now_time = datetime.now().strftime("%Y-%m-%d")
+    now_time_year = datetime.now().strftime("%Y")
+    now_time_month = datetime.now().strftime("%m")
+    print(now_time_year, now_time_month)
+    if int(now_time_year) > aunt_year:
+        print(1)
+    # 当前年份不大与2022 表示为当前年份 只看月份差值
+    else:
+        dis_month = int(now_time_month) - aunt_month
+        print(dis_month)
+    # 时间赋值
+    aunt_28date = (datetime.combine(aunt_date, time()) + timedelta(days=28 * dis_month)).strftime("%Y-%m-%d")
+    aunt_7date = datetime.strptime(aunt_28date, '%Y-%m-%d') - datetime.now()
+    print(aunt_7date.days, aunt_28date)
+    # 七天内判断
+    if 0 < aunt_7date.days < 7:
+        # aunt_data_message = "距离下一次亲戚来临还有{}天哟".format(aunt_7date.days)
+        aunt_data_message = "♥宝贝，月经可能会在{}天内来哟~预测会是{}♥".format(aunt_7date.days, aunt_28date)
+        data["data"]["aunt_data"] = {"value": aunt_data_message, "color": get_color()}
+    #
     headers = {
         'Content-Type': 'application/json',
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
@@ -200,10 +240,13 @@ def send_message(to_user, access_token, region_name, weather, temp, wind_dir, no
         print("推送消息成功")
     else:
         print(response)
- 
- 
+
+
 if __name__ == "__main__":
+    aunt_date = (datetime.combine(aunt_date, time()) + timedelta(days=28)).strftime("%Y-%m-%d")
+    print(aunt_date)
     try:
+
         with open("config.txt", encoding="utf-8") as f:
             config = eval(f.read())
     except FileNotFoundError:
@@ -214,7 +257,7 @@ if __name__ == "__main__":
         print("推送消息失败，请检查配置文件格式是否正确")
         os.system("pause")
         sys.exit(1)
- 
+
     # 获取accessToken
     accessToken = get_access_token()
     # 接收的用户
